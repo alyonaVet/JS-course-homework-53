3561277e2ed8968edba6de6d44b32517f30c838c
alyonaVet <a.v.alenka2015@gmail.com>
2024-05-23 21:51:50 +0600

    init state; added AddTaskForm, Task to App;

--- Homework53/src/App.tsx
+++ Homework53/src/App.tsx
@@ -1,8 +1,28 @@
 import './App.css';
+import {useState} from 'react';
+import {TypeTask} from './types';
+import AddTaskForm from './components/AddTaskForm/AddTaskForm';
+import Task from './components/Task/Task';
 
-const App = () => (
+const App = () => {
+  const [tasks, setTasks] = useState<TypeTask[]>([
+    {taskText: 'Do workout', id: '1'},
+    {taskText: 'Do homework', id: '2'},
+    {taskText: 'Water flowers', id: '3'},
+  ]);
+
+  return (
     <>
+      <h2>Create your tasks list:</h2>
+      <AddTaskForm/>
+      <div className="container">
+        {tasks.map((task) => {
+          return <Task key={task.id} task={task.taskText}/>
+        })}
+      </div>
     </>
-);
+  );
+};
+
 
 export default App;
